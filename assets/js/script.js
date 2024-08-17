f5ab359e76be9937d263cb928377ad39da00fbb1
D-Claffey-Code-Institute Claffey <d7x359i6@students.codeinstitute.net>
2024-08-17 21:29:47 +0000

    Add constants for code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+/* Declared constants for the rest of the code */
+const buttons = document.querySelectorAll('.controls-area button');
+const playerSelection = document.getElementById('playerSelection');
+const computerSelection = document.getElementById('computerSelection');  
+const result = document.getElementById('result');
+const winsSpan = document.getElementById('wins');
+const lossesSpan = document.getElementById('losses');
+
+let playerScore = 0;
+let computerScore = 0;
+
 
